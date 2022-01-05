ccdeb135447117e00cda554e765e9ab77f13ea5a
icorvus <business.wrona@gmail.com>
2022-01-05 23:35:22 +0100

    Done exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function(str) {
-
-};
+// Converts string to Array, reverses the Array, concenates letters
+// and turns back to string
+const reverseString = (str)  => str.split("").reverse().join('').toString()
 
 // Do not edit below this line
 module.exports = reverseString;
