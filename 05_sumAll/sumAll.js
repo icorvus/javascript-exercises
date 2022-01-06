d9c4a0340a6317f053c3fcb108d242d921180d85
icorvus <business.wrona@gmail.com>
2022-01-06 17:39:15 +0100

    Done exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+  const args = [...arguments];
+  if ( (args.length > 2) || (typeof firstNum != "number") || (typeof lastNum != "number") )
+   return "ERROR";
+  if (firstNum < 0 || lastNum < 0) return "ERROR";
+  const biggerNum = Math.max(...args);
+  const lowerNum = Math.min(...args);
 
+  // Calculated using sum of an arithmetic sequence formula
+  return (firstNum + lastNum)/2 * (biggerNum - lowerNum + 1);
 };
 
 // Do not edit below this line
