4bad9cc69145e522cd94a5ebe9deb576878aa733
icorvus <business.wrona@gmail.com>
2022-01-06 17:55:26 +0100

    Done exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,6 @@
-const ftoc = function() {
+const ftoc = (temperature) => Math.round((5/9 * (temperature - 32)) * 10) / 10;
 
-};
-
-const ctof = function() {
-
-};
+const ctof = (temperature) => Math.round((32 + 9/5 * temperature) * 10) / 10;
 
 // Do not edit below this line
 module.exports = {
