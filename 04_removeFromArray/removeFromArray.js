aac5abff67d67a14c29364c45a1a32b6461d46c3
icorvus <business.wrona@gmail.com>
2022-01-06 16:45:49 +0100

    Done exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, toRemove) {
+  let args = [...arguments];
+  for (let i = 1; i < args.length; i++) {
+    if (arr.indexOf(args[i]) === -1) continue
+    while (arr.indexOf(args[i]) != -1){
+      arr.splice(arr.indexOf(args[i]), 1);
+    };
+  };
+  return arr;
 };
 
 // Do not edit below this line
