4900ad634dbdee37a20e347ad49984df7899e8c8
icorvus <business.wrona@gmail.com>
2022-01-22 20:14:59 +0100

    Done the exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
-};
-
+const palindromes = word => {
+  let newWord = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,'');
+  newWord = newWord.replaceAll(' ', '');
+  newWord = newWord.split('');
+  newWord = newWord.reverse();
+  newWord = newWord.join('');
+  newWord = newWord.toLowerCase();
+  if (word.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,'').replaceAll(' ', '') === newWord){
+    return true;
+  } 
+  else{
+    console.log(newWord);
+    console.log(word.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,'').replaceAll(' ', ''));
+    return false;
+  } 
+}
 // Do not edit below this line
 module.exports = palindromes;
