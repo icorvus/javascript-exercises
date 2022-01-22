587937b3a12063553470a83d0448d580eb3e2c09
icorvus <business.wrona@gmail.com>
2022-01-22 19:39:01 +0100

    Done the exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,23 @@
-const add = function() {
-	
-};
+const add = (a, b) => a + b;
 
-const subtract = function() {
-	
-};
+const subtract = (a, b) => a - b;
+const sum = sumArray => {
+  let result = 0;
+  sumArray.forEach(element => result += element);
+  return result;
+}
 
-const sum = function() {
-	
-};
+const multiply = array => array.reduce((a, b) => a * b, 1);
 
-const multiply = function() {
+const power = (a, b) => a ** b;
 
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = number => {
+  let result = 1
+  for (let i = 1; i <= number; i++){
+    result *= i;
+  }
+  return result;
+}
 
 // Do not edit below this line
 module.exports = {
