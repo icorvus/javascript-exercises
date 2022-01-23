0daa1aefd8a5c10157663c7e96d7135adef9c2af
icorvus <business.wrona@gmail.com>
2022-01-23 21:53:02 +0100

    Done the exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = (array) => {
+  return array.reduce((personA, personB) => {
+    if (!("yearOfDeath" in personA)) personA.yearOfDeath = new Date().getFullYear();
+    if (!("yearOfDeath" in personB)) personB.yearOfDeath = new Date().getFullYear();
 
+    if (personB.yearOfDeath - personB.yearOfBirth > personA.yearOfDeath - personA.yearOfBirth){
+      return personB;
+    } else {
+      return personA;
+    }
+  });
 };
 
 // Do not edit below this line
