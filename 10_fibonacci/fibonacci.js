0ddbab604afec5cc3475023b846853629c31fcbd
icorvus <business.wrona@gmail.com>
2022-01-23 18:39:08 +0100

    Done the exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (n < 0){
+    return 'OOPS';
+  }
+  let a = 0;
+  let b = 1;
+  let c = 1;
+  if(n < 3) {
+    return 1;
+  } else {
+    for (let i = 2; i < n; i++){
+      a = b;
+      b = c;
+      c = a + b;
+    }
+    return c;
+  }
 };
 
 // Do not edit below this line
